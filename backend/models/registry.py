4d6030ebdfd6b63047bef28488b7c0e915ce734f
cylin0201 <cylin0201@naver.com>
2025-06-06 17:04:12 +0900

    새로운 모델 연결

--- backend/models/registry.py
+++ backend/models/registry.py
@@ -1,7 +1,9 @@
+#from .openpsi_model_v2 import OpenPsiModelv2
 from .openpsi_model import OpenPsiModel
 from .gpt4o_model import GPT4OModel
 
 model_registry = {
+    #"openpsiv2": OpenPsiModelv2(),
     "openpsi": OpenPsiModel(),
     "gpt4o": GPT4OModel()
 }
